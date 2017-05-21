f91c5bbdca290ee916e8980116c3c4af7eb167e6
Paul Andrieux <paul@troopers.email>
2017-05-21 23:29:39 +0200

    fix compatibility with twig 2.0

--- Twig/AsseticInjectorTokenParser.php
+++ Twig/AsseticInjectorTokenParser.php
@@ -9,19 +9,18 @@
  * with this source code in the file LICENSE.
  */
 
-namespace AppVentus\AsseticInjectorBundle\Twig;
+namespace Troopers\AsseticInjectorBundle\Twig;
 
 use Assetic\Asset\AssetInterface;
-use Symfony\Bundle\AsseticBundle\Twig\AsseticTokenParser as BaseAsseticTokenParser;
 use Symfony\Bundle\AsseticBundle\Exception\InvalidBundleException;
-use Symfony\Component\Templating\TemplateNameParserInterface;
 use Symfony\Bundle\AsseticBundle\Factory\AssetFactory;
 use Symfony\Bundle\AsseticBundle\Twig\AsseticNode;
+use Symfony\Bundle\AsseticBundle\Twig\AsseticTokenParser as BaseAsseticTokenParser;
+use Symfony\Component\Templating\TemplateNameParserInterface;
+use Symfony\Bundle\FrameworkBundle\Templating\TemplateReference;
 
 /**
  * Assetic token parser.
- *
- * @author Kris Wallsmith <kris@symfony.com>
  */
 class AsseticInjectorTokenParser extends BaseAsseticTokenParser
 {
@@ -29,15 +28,14 @@ class AsseticInjectorTokenParser extends BaseAsseticTokenParser
     private $enabledBundles;
     private $injectedAssets;
 
-    public function __construct(AssetFactory $factory, $tag, $output, $single = false, array $extensions = array())
+    public function __construct(AssetFactory $factory, $tag, $output, $single = false, array $extensions = [])
     {
-
-        $this->factory    = $factory;
-        $this->tag        = $tag;
-        $this->output     = $output;
-        $this->single     = $single;
+        $this->factory = $factory;
+        $this->tag = $tag;
+        $this->output = $output;
+        $this->single = $single;
         $this->extensions = $extensions;
-        $this->injectedAssets = array();
+        $this->injectedAssets = [];
 
         parent::__construct($factory, $tag, $output, $single, $extensions);
     }
@@ -46,6 +44,7 @@ class AsseticInjectorTokenParser extends BaseAsseticTokenParser
     {
         $this->injectedAssets = $assets;
     }
+
     public function setTemplateNameParser(TemplateNameParserInterface $templateNameParser)
     {
         $this->templateNameParser = $templateNameParser;
@@ -62,7 +61,7 @@ class AsseticInjectorTokenParser extends BaseAsseticTokenParser
             // check the bundle
             $templateRef = null;
             try {
-                $templateRef = $this->templateNameParser->parse($this->parser->getStream()->getFilename());
+                $templateRef = $this->templateNameParser->parse($this->parser->getStream()->getSourceContext()->getName());
             } catch (\RuntimeException $e) {
                 // this happens when the filename isn't a Bundle:* url
                 // and it contains ".."
@@ -70,11 +69,7 @@ class AsseticInjectorTokenParser extends BaseAsseticTokenParser
                 // this happens when the filename isn't a Bundle:* url
                 // but an absolute path instead
             }
-            try {
-                $bundle = $templateRef ? $templateRef->get('bundle') : null;
-            } catch (\InvalidArgumentException $e) {
-                $bundle = null;
-            }
+            $bundle = $templateRef instanceof TemplateReference ? $templateRef->get('bundle') : null;
             if ($bundle && !in_array($bundle, $this->enabledBundles)) {
                 throw new InvalidBundleException($bundle, "the {% {$this->getTag()} %} tag", $templateRef->getLogicalName(), $this->enabledBundles);
             }
@@ -83,14 +78,14 @@ class AsseticInjectorTokenParser extends BaseAsseticTokenParser
         return $this->parseAndInject($token);
     }
 
-    protected function createNode(AssetInterface $asset, \Twig_NodeInterface $body, array $inputs, array $filters, $name, array $attributes = array(), $lineno = 0, $tag = null)
+    protected function createNode(AssetInterface $asset, \Twig_Node $body, array $inputs, array $filters, $name, array $attributes = [], $lineno = 0, $tag = null)
     {
         return new AsseticNode($asset, $body, $inputs, $filters, $name, $attributes, $lineno, $tag);
     }
 
     public function parseAndInject(\Twig_Token $token)
     {
-        $inputs = $filters = $injectorLocationsAvailables = array();
+        $inputs = $filters = $injectorLocationsAvailables = [];
         $name = $injectorLocation = null;
 
         foreach ($this->injectedAssets as $tag => $assets) {
@@ -99,11 +94,11 @@ class AsseticInjectorTokenParser extends BaseAsseticTokenParser
             }
         }
 
-        $attributes = array(
+        $attributes = [
             'output'   => $this->output,
             'var_name' => 'asset_url',
-            'vars'     => array(),
-        );
+            'vars'     => [],
+        ];
 
         $stream = $this->parser->getStream();
         while (!$stream->test(\Twig_Token::BLOCK_END_TYPE)) {
@@ -139,12 +134,12 @@ class AsseticInjectorTokenParser extends BaseAsseticTokenParser
                 // debug=true
                 $stream->next();
                 $stream->expect(\Twig_Token::OPERATOR_TYPE, '=');
-                $attributes['debug'] = 'true' == $stream->expect(\Twig_Token::NAME_TYPE, array('true', 'false'))->getValue();
+                $attributes['debug'] = 'true' == $stream->expect(\Twig_Token::NAME_TYPE, ['true', 'false'])->getValue();
             } elseif ($stream->test(\Twig_Token::NAME_TYPE, 'combine')) {
                 // combine=true
                 $stream->next();
                 $stream->expect(\Twig_Token::OPERATOR_TYPE, '=');
-                $attributes['combine'] = 'true' == $stream->expect(\Twig_Token::NAME_TYPE, array('true', 'false'))->getValue();
+                $attributes['combine'] = 'true' == $stream->expect(\Twig_Token::NAME_TYPE, ['true', 'false'])->getValue();
             } elseif ($stream->test(\Twig_Token::NAME_TYPE, 'vars')) {
                 // vars=['locale','browser']
                 $stream->next();
@@ -174,12 +169,12 @@ class AsseticInjectorTokenParser extends BaseAsseticTokenParser
         }
         $stream->expect(\Twig_Token::BLOCK_END_TYPE);
 
-        $body = $this->parser->subparse(array($this, 'testEndTag'), true);
+        $body = $this->parser->subparse([$this, 'testEndTag'], true);
 
         $stream->expect(\Twig_Token::BLOCK_END_TYPE);
 
         if ($injectorLocation) {
-            $injectorLocationArray = explode(",", $injectorLocation);
+            $injectorLocationArray = explode(',', $injectorLocation);
 
             //INJECT
             foreach ($injectorLocationArray as $injectorLocation) {
@@ -187,7 +182,7 @@ class AsseticInjectorTokenParser extends BaseAsseticTokenParser
                 if (array_key_exists($this->tag, $this->injectedAssets) && in_array($injectorLocation, $injectorLocationsAvailables)) {
                     if (!empty($this->injectedAssets[$this->tag][$injectorLocation])) {
                         if (!is_array($this->injectedAssets[$this->tag][$injectorLocation])) {
-                            $this->injectedAssets[$this->tag][$injectorLocation] = array($this->injectedAssets[$this->tag][$injectorLocation]);
+                            $this->injectedAssets[$this->tag][$injectorLocation] = [$this->injectedAssets[$this->tag][$injectorLocation]];
                         }
                         $inputs = array_merge($inputs, $this->injectedAssets[$this->tag][$injectorLocation]);
                     }
@@ -202,7 +197,7 @@ class AsseticInjectorTokenParser extends BaseAsseticTokenParser
             $name = $this->factory->generateAssetName($inputs, $filters, $attributes);
         }
 
-        $asset = $this->factory->createAsset($inputs, $filters, $attributes + array('name' => $name));
+        $asset = $this->factory->createAsset($inputs, $filters, $attributes + ['name' => $name]);
 
         return $this->createNode($asset, $body, $inputs, $filters, $name, $attributes, $token->getLine(), $this->getTag());
     }
